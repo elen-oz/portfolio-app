65a15ab03d8f1e5b861d4dddc13f3c1a26a2c5df
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-13 17:44:23 +0200

    feat: add show/hide navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assets/logo_dark1.png';
 
 function NavBar() {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-stone-100 text-stone-500'>
       <div>
@@ -22,17 +27,26 @@ function NavBar() {
       </ul>
 
       {/* hamburger */}
-      <div className='md:hidden'>
+      <div
+        onClick={handleClick}
+        className='md:hidden'
+      >
         <FaBars />
       </div>
 
       {/* mobile menu */}
-      <ul className='absolute top-0 left-0 h-screen bg-stone-100 flex flex-col justify-center items-center'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 h-screen bg-stone-100 flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
       </ul>
 
       {/* social icons */}
