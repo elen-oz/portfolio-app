3f9a3912afde349cfa7d0e17a4d8755bd0a20c58
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-14 11:39:22 +0200

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='w-full  bg-neutral-50 text-neutral-600 flex justify-center items-center pb-4'>
+    <div className='w-full h-[80px] bg-neutral-50 text-neutral-600 flex justify-center items-center -mt-20'>
       <ul className='flex'>
         <li>Elena Ozerova &copy;</li>
         <li>{new Date().getFullYear()}</li>
