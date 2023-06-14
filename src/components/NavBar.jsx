82d5a396958c201936b0a5f1df9f330ec140046b
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-14 09:38:44 +0200

    change social links on the left

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -95,7 +95,7 @@ function NavBar() {
       </ul>
 
       {/* social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className='hidden md:flex fixed flex-col top-[35%] md:top-[14%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-neutral-50'>
             <a
