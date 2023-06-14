5af0d4a343b74a680bc52adb2f72f7b816c0b75c
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-14 09:59:29 +0200

    add links to mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -87,11 +87,56 @@ function NavBar() {
             : 'absolute top-0 left-0 w-full h-screen bg-neutral-50 flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl hover:text-red-900'>Home</li>
-        <li className='py-6 text-4xl hover:text-red-900'>About</li>
-        <li className='py-6 text-4xl hover:text-red-900'>Skills</li>
-        <li className='py-6 text-4xl hover:text-red-900'>Work</li>
-        <li className='py-6 text-4xl hover:text-red-900'>Contact</li>
+        <li className='py-6 text-4xl hover:text-red-900'>
+          <Link
+            onClick={handleClick}
+            to='home'
+            smooth={true}
+            duration={500}
+          >
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl hover:text-red-900'>
+          <Link
+            onClick={handleClick}
+            to='about'
+            smooth={true}
+            duration={500}
+          >
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl hover:text-red-900'>
+          <Link
+            onClick={handleClick}
+            to='skills'
+            smooth={true}
+            duration={500}
+          >
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl hover:text-red-900'>
+          <Link
+            onClick={handleClick}
+            to='work'
+            smooth={true}
+            duration={500}
+          >
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl hover:text-red-900'>
+          <Link
+            onClick={handleClick}
+            to='contact'
+            smooth={true}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/* social icons */}
