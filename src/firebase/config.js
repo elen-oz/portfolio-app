97aea8221f5cc6f6ede855dc0752b1d2e3e3b9c3
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-14 15:01:26 +0200

    deploy on firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBra8qSqxlDTwlKi5I0aIepdyYkcPCWuVY',
+  authDomain: 'portfolio-app-c7ac7.firebaseapp.com',
+  projectId: 'portfolio-app-c7ac7',
+  storageBucket: 'portfolio-app-c7ac7.appspot.com',
+  messagingSenderId: '832743826529',
+  appId: '1:832743826529:web:bb86cbd2155d425ac73620',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
