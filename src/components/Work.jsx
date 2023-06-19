d77211b40225690ceac5cc7cae29e45e670f5877
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-19 11:21:57 +0200

    refactor work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import { useTheme } from '../hooks/useTheme';
 import ProjectList from './ProjectList';
+import data from '../data/data';
 
 const Work = () => {
+  // turn it on when turn on the firebase
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
   const { darkMode } = useTheme();
 
   return (
-    <div className={darkMode ? 'dark' : ''}>
-      <ProjectList />
+    <div
+      id='work'
+      className={darkMode ? 'dark' : ''}
+    >
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <ProjectList data={data} />}
     </div>
   );
 };
