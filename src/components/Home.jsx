6f2ff1c4eea09628cb872e5d28ef498732dec8da
elen-oz <ozerova.lenka@gmail.com>
2023-11-24 17:59:28 +0100

    feat: divide css files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,18 @@ import { useTheme } from '../hooks/useTheme';
 import { Link } from 'react-scroll';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import MyName from '../assets/my-name.png';
-import styles from '../styles/main.module.css';
+import mainStyles from '../styles/main.module.css';
+import styles from './Home.module.css';
 
 const Home = () => {
   const { darkMode } = useTheme();
 
   return (
-    <div className={darkMode ? styles.darkTheme : ''}>
-      <div name='home' className={styles.homeComponent}>
+    <div className={darkMode ? mainStyles.darkTheme : ''}>
+      <div name='home' className={styles.home}>
         {/* container */}
-        {/* <div className='container max-w-[900px] mx-auto flex flex-col justify-center h-full'> */}
         <div className={styles.container}>
-          <p className={styles.textAccent}>Hi! I am</p>
+          <p className={styles.greeting}>Hi! I am</p>
           {/* <h1 className='text-4xl sm:text-7xl font-bold'>Elena</h1> */}
           <h1>
             <img
@@ -23,7 +23,6 @@ const Home = () => {
             />
           </h1>
 
-          {/* <h2 className='text-neutral-500 text-4xl sm:text-7xl font-bold'> */}
           <h2 className={styles.subtitle}>
             and I love <br /> programming
           </h2>
