df6961bdb6f21bdea0707c2c42cc44689b934218
elen-oz <ozerova.lenka@gmail.com>
2023-11-24 17:29:56 +0100

    feat: finish hero/home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,19 +2,18 @@ import { useTheme } from '../hooks/useTheme';
 import { Link } from 'react-scroll';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import MyName from '../assets/my-name.png';
+import styles from '../styles/main.module.css';
 
 const Home = () => {
   const { darkMode } = useTheme();
 
   return (
-    <div className={darkMode ? 'dark' : ''}>
-      <div
-        name='home'
-        className='w-full h-screen dark:bg-gray-800 dark:text-neutral-300'
-      >
+    <div className={darkMode ? styles.darkTheme : ''}>
+      <div name='home' className={styles.homeComponent}>
         {/* container */}
-        <div className='container max-w-[900px] mx-auto flex flex-col justify-center h-full'>
-          <p className='text-red-700 text-xl'>Hi! I am</p>
+        {/* <div className='container max-w-[900px] mx-auto flex flex-col justify-center h-full'> */}
+        <div className={styles.container}>
+          <p className={styles.textAccent}>Hi! I am</p>
           {/* <h1 className='text-4xl sm:text-7xl font-bold'>Elena</h1> */}
           <h1>
             <img
@@ -24,19 +23,18 @@ const Home = () => {
             />
           </h1>
 
-          <h2 className='text-neutral-500 text-4xl sm:text-7xl font-bold '>
+          {/* <h2 className='text-neutral-500 text-4xl sm:text-7xl font-bold'> */}
+          <h2 className={styles.subtitle}>
             and I love <br /> programming
           </h2>
-          <p className='py-4 max-w-[700px]'>
-            An enthusiastic <strong>Frontend Developer</strong> based in Stockholm, Sweden.
+
+          <p className={styles.description}>
+            An enthusiastic <strong>Frontend Developer</strong> based in
+            Stockholm, Sweden.
           </p>
           <div>
-            <Link
-              to='work'
-              smooth={true}
-              duration={500}
-            >
-              <button className='group border-2 px-6 py-3 my-2 flex items-center border-red-700 hover:text-stone-50 hover:bg-red-700 hover:border-red-700'>
+            <Link to='work' smooth={true} duration={500}>
+              <button className={styles.button}>
                 View Work
                 <HiArrowNarrowRight className='ml-3' />
               </button>
