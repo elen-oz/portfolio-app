1b140defbd3e2410bbc6c0f33a0e896e138c4903
elen-oz <ozerova.lenka@gmail.com>
2024-01-31 22:13:29 +0100

    feat: add notion link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,15 @@ const Footer = () => {
           </a>
         </li>
         <li>{new Date().getFullYear()}</li>
+        <li>
+          <a
+            href='https://www.notion.so/Portfolio-of-Elena-bbfdf9a7e6c54669829923d14b6cd7f6?pvs=4'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Notion
+          </a>
+        </li>
       </ul>
     </div>
   );
